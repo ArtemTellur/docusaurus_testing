db1a164bfbd643380232098d2c305e49de079b22
Artem Kolomin <kolominartem@gmail.com>
2023-12-26 14:34:16 +0400

    removed blog;

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,13 +46,6 @@ const config = {
           editUrl:
             'https://github.com/ArtemTellur/docusaurus_testing/tree/main',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/ArtemTellur/docusaurus_testing/tree/main',
-        },
         theme: {
           customCss: './src/css/custom.css',
         },
